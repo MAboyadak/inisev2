1ae9d00f282be3840e8a7806d66f6451b789d11e
Mohamed-Aboyadak <mohamedhamdy9711@gmail.com>
2023-05-07 19:17:46 +0300

    fix err

--- conf/db.php
+++ conf/db.php
@@ -12,7 +12,7 @@ class DB
             return self::$conn;
         }
         try{
-            self::$conn = new \PDO('mysql:host=91.107.196.153;port=3306;dbname='.DB_NAME, DB_USER, DB_PASS,[
+            self::$conn = new \PDO('mysql:host=http://test84.interview.inisev.com/;port=3306;dbname='.DB_NAME, DB_USER, DB_PASS,[
                 \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
             ]);
