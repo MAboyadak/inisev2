a5b430dc289a8e86b7416e296bf115a039f3e21d
Mohamed-Aboyadak <mohamedhamdy9711@gmail.com>
2023-05-07 19:45:53 +0300

    fix err

--- conf/config.php
+++ conf/config.php
@@ -5,7 +5,7 @@ include_once __DIR__.'/../helpers/auth.php';
 include_once __DIR__.'/../helpers/validation.php';
 
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Mcjepf9xccHdFmgNrjT3AT');
 define('DB_NAME', 'inisev_task');
 
 ?>
