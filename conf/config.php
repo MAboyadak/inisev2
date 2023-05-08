d4fd4f1d4b63a15e772fc7403bca89e4b5305055
Mohamed-Aboyadak <mohamedhamdy9711@gmail.com>
2023-05-08 19:44:57 +0300

    fix - linux(case sensetive)

--- conf/config.php
+++ conf/config.php
@@ -4,7 +4,7 @@ include_once __DIR__.'/../helpers/sql.php';
 include_once __DIR__.'/../helpers/auth.php';
 include_once __DIR__.'/../helpers/validation.php';
 
-define('DB_USER', 'maboyadak');
+define('DB_USER', 'mhamdy');
 define('DB_PASS', '123456789');
 define('DB_NAME', 'inisev_task');
 
